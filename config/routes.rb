f3f7bd418189ea34fbe0ca0ee5f4de36754811e1
Prescott Murphy <prurph@gmail.com>
2014-03-21 18:20:58 -0400

    Add username to audio players

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,16 @@
 AudioRecordRails::Application.routes.draw do
   devise_for :users
 
-  # resources :readings, only: [:create, :show]
+  resources :readings, only: :show
   resources :poems, except: [:destroy, :update] do
     resources :readings, only: [:index, :create]
   end
-
-  get '/readings/:id', to: 'readings#show'
   # post 'audio/save_file'
   # get  'audio/get_file/:id', to: 'audio#get_file'
+  # get  'audio/index'
 
   post   'favorites/:type/:id', to: 'favorites#create', as: 'new_favorite'
   delete 'favorites/:type/:id', to: 'favorites#destroy', as: 'destroy_favorite'
-  get  'audio/index'
+
   root 'static_pages#home'
 end
